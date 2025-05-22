592734c6a0e50eb1067868b7ba7b67f47dc99597
jc.melendrezz <jomarmelendrez18@gmail.com>
2025-05-22 13:17:18 +0800

    Integrate external app SMS(textbee)

--- meditrack/reset_reminders.bat
+++ meditrack/reset_reminders.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d "C:\Users\Acer\meditrack\integ_final_project\meditrack"
-call C:\Users\Acer\meditrack\env\Scripts\activate.bat
+cd /d "C:\Users\Acer\meditrack_new\integ_final_project\meditrack"
+call C:\Users\Acer\meditrack_new\env\Scripts\activate.bat
 python manage.py reset_reminders >> rest_reminders_log.txt 2>&1
